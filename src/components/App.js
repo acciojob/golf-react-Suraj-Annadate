6a4e89be42c73ce0c710ffee419372b8cea5c9b7
suraj0698 <annadatesuraj@gmail.com>
2023-10-16 18:11:26 +0530

    c2

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,42 @@
-import React, {useState } from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Ball from "./Ball";
 
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderBall: false,
+            posi : 0,
+            ballPosition: { left: "0px" }
+        };
+        this.renderChoice = this.renderBallOrButton.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+    };
 
+    buttonClickHandler() {
+   
+   }
+    renderBallOrButton() {
+		if (this.state.renderBall) {
+		    return <div className="ball" style={this.state.ballPosition}></div>
+		} else {
+		    return <button onClick={this.buttonClickHandler} >Start</button>
+		}
+    }
 
-const App = ()=>{
+    // bind ArrowRight keydown event
+    componentDidMount() {
+      
+    }
 
-    let [btnclicked,setBtnclicked] = useState(false);
-    
-    return (<div className="playground">
-         {(!btnclicked) && <button className="start" onClick={()=>{setBtnclicked(true)}}>Start</button>}
-         <Ball btnclicked={btnclicked} setBtnclicked={setBtnclicked} />
-    </div>)
+    render() {
+        return (
+            <div className="playground">
+                {this.renderBallOrButton()}
+            </div>
+        )
+    }
 }
+
 
 export default App;
